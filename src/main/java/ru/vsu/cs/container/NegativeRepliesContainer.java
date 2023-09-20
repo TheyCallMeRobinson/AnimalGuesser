a8d7455909ca32e2c6564a59a6c08ece2e946cc3
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2023-09-21 00:13:02 +0300

    Create negative and positive replies containers

--- src/main/java/ru/vsu/cs/container/NegativeRepliesContainer.java
+++ src/main/java/ru/vsu/cs/container/NegativeRepliesContainer.java
@@ -1,4 +1,22 @@
 package ru.vsu.cs.container;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Setter
+@Getter
 public class NegativeRepliesContainer {
+
+    public List<String> values;
+
+    public NegativeRepliesContainer(List<String> customReplies) {
+        values = customReplies;
+    }
+
+    public NegativeRepliesContainer() {
+        values = Arrays.asList("нет", "no", "-");
+    }
 }
--- src/main/java/ru/vsu/cs/container/PositiveRepliesContainer.java
+++ src/main/java/ru/vsu/cs/container/PositiveRepliesContainer.java
@@ -1,4 +1,22 @@
 package ru.vsu.cs.container;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Setter
+@Getter
 public class PositiveRepliesContainer {
+
+    public List<String> values;
+
+    public PositiveRepliesContainer(List<String> customReplies) {
+        values = customReplies;
+    }
+
+    public PositiveRepliesContainer() {
+        values = Arrays.asList("да", "yes", "+");
+    }
 }
