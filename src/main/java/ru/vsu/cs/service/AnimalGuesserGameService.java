18bea8cda9133bd5cab891da52e65c9a5e4ee286
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2023-09-21 00:00:27 +0300

    Create basic fields and methods structure in GameService class

--- src/main/java/ru/vsu/cs/service/AnimalGuesserGameService.java
+++ src/main/java/ru/vsu/cs/service/AnimalGuesserGameService.java
@@ -1,4 +1,35 @@
 package ru.vsu.cs.service;
 
+import ru.vsu.cs.controller.InputController;
+import ru.vsu.cs.controller.OutputController;
+import ru.vsu.cs.model.AnimalNode;
+import ru.vsu.cs.model.GameStartNode;
+
 public class AnimalGuesserGameService {
+
+    private GameStartNode gameStartNode;
+    private InputController inputController;
+    private OutputController outputController;
+    private AnimalNode currentAnimal;
+    private Boolean gameOverFlag;
+
+    public AnimalGuesserGameService(GameStartNode startAnimal) {
+        gameStartNode = startAnimal;
+        gameOverFlag = false;
+    }
+
+    public void invokeService() {}
+
+    private void onNewGameStart() {}
+
+    private void onNextQuestion() {}
+
+    private void onNegativeResponse() {}
+
+    private void onPositiveResponse() {}
+
+    private void onGameLoose() {}
+
+    private void onGameOver() {}
+
 }
